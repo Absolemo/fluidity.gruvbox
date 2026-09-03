95ea81bd4d71989fc9e1220d8ad27d2f2aaffcc6
Absolemo <dazedaninetyfivegmx.de>
2026-09-03 18:11:32 +0200

    change placeholder

--- src/Startpage/Searchbar/Searchbar.tsx
+++ src/Startpage/Searchbar/Searchbar.tsx
@@ -76,7 +76,7 @@ export const Searchbar = () => {
     <StyledSearchbarContainer>
       {searchSymbol && <SearchIcon src={searchSymbol} />}
       <StyledSearchbar
-        placeholder="Always stay clean!"
+        placeholder="#ERROR: Please Enter Text!"
         type="input"
         onKeyUp={e =>
           e.key === "Enter" && redirectToSearch(e.currentTarget.value)
